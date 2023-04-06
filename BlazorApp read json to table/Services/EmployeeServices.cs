3e09b3abeb06fb10633c4c5a3bfd09488f278096
krant <kranthireddy.mekala@gmail.com>
2023-04-07 02:19:46 +0530

    Add project files.

---
+++ BlazorApp read json to table/Services/EmployeeServices.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace BlazorApp_read_json_to_table.Services
+{
+    public class EmployeeServices
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeServices(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Dictionary<string,object>>> GetEmployees()
+        {
+            var response = await httpClient.GetAsync("https://localhost:44359/api/employees");
+            var json = await response.Content.ReadAsStringAsync();
+            var jsonDocumnet = JsonDocument.Parse(json);
+
+            var Employees = new List<Dictionary<string,object>>();
+
+            foreach(var jsonElement in jsonDocumnet.RootElement.EnumerateArray()) {
+                var Employe = new Dictionary<string,object>();
+
+                foreach(var property in jsonElement.EnumerateObject())
+                {
+                    Employe.Add(property.Name, property.Value);
+                }
+
+                Employees.Add(Employe);
+
+            }
+            return Employees;
+        }
+    }
+}
---
+++ BlazorApp read json to table/wwwroot/my-functions.js
@@ -0,0 +1,24 @@
+﻿function BlazorDownloadFile(fileName, contentType, content) {
+    // Convert content from base64 to binary data
+    const binaryData = atob(content);
+
+    // Create a new blob object
+    const blob = new Blob([binaryData], { type: contentType });
+
+    // Create a temporary URL to the blob
+    const url = URL.createObjectURL(blob);
+
+    // Create a link element to trigger the download
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    // Revoke the URL to free up memory
+    URL.revokeObjectURL(url);
+}
+
+
+
